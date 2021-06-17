d470180450aeb1ff258bd64caa81241a26200695
jindong.huang <jindong.huang@weride.ai>
2021-06-17 15:56:49 +0800

    init project with rollup and tsc, add format date util

---
+++ src/date.ts
@@ -0,0 +1,6 @@
+export const formatDate = (date: Date) =>
+  `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(
+    date.getDate()
+  ).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(
+    date.getMinutes()
+  ).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`
